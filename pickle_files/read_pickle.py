a04ac620897501c052594c9fe0b78dfbe53cf923
Jakob Freiberger <freibja@gmail.com>
2025-05-21 01:04:05 +0900

    correct total_frames in pickle

--- pickle_files/read_pickle.py
+++ pickle_files/read_pickle.py
@@ -2,7 +2,4 @@ import pickle
 
 with open("fitness-aqa.pkl", mode="rb") as f:
     dict = pickle.load(f)
-
-    for entry in dict["annotations"]:
-        if entry["frame_dir"] == "66189_5":
-            pass
+    pass
